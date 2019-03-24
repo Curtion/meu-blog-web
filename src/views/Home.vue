9bc45b82b921978a00e77237d0ac7cb2262f7f79
Curtion <curtion@126.com>
2019-03-24 17:06:21 +0800

    布局

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,82 +1,54 @@
 <template>
-  <div class="home">
+  <div id="body">
+    <div class="home" type="flex" justify="center">
+        <el-container>
+            <el-header class="header">
+                <header-meu></header-meu>
+            </el-header>
+            <el-main class="main">内容</el-main>
+        </el-container>
+    </div>
     <el-container>
-      <el-aside class="aside" width="300px">
-        <el-menu default-active="1-4-1" class="el-menu-vertical-demo" style="height:100vh;" :collapse="isCollapse">
-            <el-submenu index="1">
-                <template slot="title">
-                <i class="el-icon-location"></i>
-                <span slot="title">导航一</span>
-                </template>
-                <el-menu-item-group>
-                <span slot="title">分组一</span>
-                <el-menu-item index="1-1">选项1</el-menu-item>
-                <el-menu-item index="1-2">选项2</el-menu-item>
-                </el-menu-item-group>
-                <el-menu-item-group title="分组2">
-                <el-menu-item index="1-3">选项3</el-menu-item>
-                </el-menu-item-group>
-                <el-submenu index="1-4">
-                <span slot="title">选项4</span>
-                <el-menu-item index="1-4-1">选项1</el-menu-item>
-                </el-submenu>
-            </el-submenu>
-            <el-menu-item index="2">
-                <i class="el-icon-menu"></i>
-                <span slot="title">导航二</span>
-            </el-menu-item>
-            <el-menu-item index="3" disabled>
-                <i class="el-icon-document"></i>
-                <span slot="title">导航三</span>
-            </el-menu-item>
-            <el-menu-item index="4">
-                <i class="el-icon-setting"></i>
-                <span slot="title">导航四</span>
-            </el-menu-item>
-        </el-menu>
-        <i class="el-icon-menu menu"></i>
-      </el-aside>
-      <el-aside class="aside2" width="300px">侧边栏</el-aside>
-      <el-container>
-        <el-header class="header">头部导航</el-header>
-        <el-main class="main">内容</el-main>
         <el-footer class="footer">底部导航</el-footer>
-      </el-container>
     </el-container>
   </div>
 </template>
 <script>
+import header from './components/header.vue'
 export default {
-    data() {
-        return {
-            isCollapse: false
-        }
+    name: 'home',
+    components: {
+        headerMeu: header
     }
-}
+};
 </script>
 
 <style scoped>
-    .menu {
-        position: absolute;
-        right: 0px;
-        top: 0px;
-        font-size: 32px;
+    @media screen and (max-width:1200px) and (min-width:1024px){
+        .home{
+            width:1024px;
+            margin:0 auto;
+            position: relative;
+        }
     }
-    .aside {
-        position: relative;
-        height: 100vh;
+    @media screen and (max-width:1024px){
+        .home{
+            width:960px;
+            margin:0 auto;
+            position: relative;
+        }
     }
-    .aside2 {
-        height: 100vh;
-        background-color: #AAA;
+    @media screen and (min-width:1200px){
+        .home{
+            width:1200px;
+            margin:0 auto;
+            position: relative;
+        }
     }
     .header {
-        background-color: #EEE;
-    }
-    .main {
-        background-color: green;
+        border-bottom: 1px solid #dcdfe6;
     }
     .footer {
-        background-color: #D11;
+        border-top: 1px solid #dcdfe6;
     }
 </style>
---
+++ src/views/components/header.vue
@@ -0,0 +1,38 @@
+<template>
+import func from './vue-temp/vue-editor-bridge';
+    <div class="header-meu">
+        <span>竹影流浪</span>
+        <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+            <el-menu-item index="1">首页</el-menu-item>
+            <el-menu-item index="1">归档</el-menu-item>
+            <el-menu-item index="1">标签</el-menu-item>
+            <el-submenu index="2">
+                <template slot="title">我的工作台</template>
+                <el-menu-item index="2-1">选项1</el-menu-item>
+                <el-menu-item index="2-2">选项2</el-menu-item>
+                <el-menu-item index="2-3">选项3</el-menu-item>
+                <el-submenu index="2-4">
+                <template slot="title">选项4</template>
+                <el-menu-item index="2-4-1">选项1</el-menu-item>
+                <el-menu-item index="2-4-2">选项2</el-menu-item>
+                <el-menu-item index="2-4-3">选项3</el-menu-item>
+                </el-submenu>
+            </el-submenu>
+        </el-menu>
+    </div>
+</template>
+<script>
+export default {
+    name: 'headerMeu',
+    data:() => {
+        return {
+            webSiteName: "竹影流浪"
+        }
+    },
+    methods: {
+        handleSelect(key, handle) {
+            console.log(key);
+        }
+    }
+}
+</script>
