6b069da8f864f079f056fff3652ac4e5ec388287
Curtion <curtion@126.com>
2019-04-09 23:22:44 +0800

    评论样式

--- src/views/post/components/messages/index.vue
+++ src/views/post/components/messages/index.vue
@@ -1,35 +1,41 @@
 <template>
     <div class="msg">
-        <el-form :inline="true" :model="formInline" class="demo-form-inline">
-            <el-form-item label="审批人">
-                <el-input v-model="formInline.user" placeholder="审批人"></el-input>
-            </el-form-item>
-            <el-form-item label="活动区域">
-                <el-select v-model="formInline.region" placeholder="活动区域">
-                <el-option label="区域一" value="shanghai"></el-option>
-                <el-option label="区域二" value="beijing"></el-option>
-                </el-select>
-            </el-form-item>
-            <el-form-item>
-                <el-button type="primary" @click="onSubmit">查询</el-button>
-            </el-form-item>
-        </el-form>
+        <el-row :gutter="24">
+            <el-col :span="8"><el-input v-model="form.user" placeholder="用户名*"></el-input></el-col>
+            <el-col :span="8"><el-input v-model="form.email" placeholder="邮箱"></el-input></el-col>
+            <el-col :span="8"><el-input v-model="form.email" placeholder="主页"></el-input></el-col>
+        </el-row>
+        <div style="margin: 10px 0;"></div>
+        <el-input
+        type="textarea"
+        :autosize="{ minRows: 2, maxRows: 4}"
+        placeholder="请输入内容"
+        v-model="form.content">
+        </el-input>
+        <div style="margin: 20px 0;"></div>
+        <div class="subbtn">
+            <el-button type="info">提交评论</el-button>
+        </div>
     </div>
 </template>
 <script>
 export default {
     data() {
         return {
-            formInline: {
+            form: {
                 user: '',
-                region: ''
+                email: ''
             }
         }
     },
     methods: {
-        onSubmit() {
-            console.log('submit!');
-        }
     }
 }
 </script>
+
+<style scoped>
+.subbtn {
+    display: flex;
+    justify-content: flex-end;
+}
+</style>
