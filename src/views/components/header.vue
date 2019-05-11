1f40654b2b2829e643b3d5321f5f967f8a95ab57
Curtion <curtion@126.com>
2019-05-11 21:57:50 +0800

    顶部导航

--- src/views/components/header.vue
+++ src/views/components/header.vue
@@ -6,15 +6,15 @@
             <el-menu-item index="3" :route="{path: '/tags'}">标签</el-menu-item>
             <el-submenu index="4">
                 <template slot="title">关于</template>
-                <el-menu-item index="4-1" :route="{path: '/works'}">作品</el-menu-item>
+                <!-- <el-menu-item index="4-1" :route="{path: '/works'}">作品</el-menu-item>
                 <el-menu-item index="4-2" :route="{path: '/books'}">书籍</el-menu-item>
                 <el-menu-item index="4-3" :route="{path: '/movie'}">电影</el-menu-item>
                 <el-menu-item index="4-4" :route="{path: '/music'}">音乐</el-menu-item>
                 <el-submenu index="4-5">
-                <template slot="title">About Me</template>
+                <template slot="title">About Me</template> -->
                 <el-menu-item index="4-5-1" :route="{path: '/about-me'}">关于我</el-menu-item>
-                <el-menu-item index="4-5-1" :route="{path: '/resume'}">简历</el-menu-item>
-                <el-menu-item index="4-5-2" :route="{path: '/coffee'}">打赏</el-menu-item>
+                <!-- <el-menu-item index="4-5-1" :route="{path: '/resume'}">简历</el-menu-item>
+                <el-menu-item index="4-5-2" :route="{path: '/coffee'}">打赏</el-menu-item> -->
                 </el-submenu>
             </el-submenu>
         </el-menu>
